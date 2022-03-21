51777dc9d637bf315c5b10eca5e99ee3b28d4852
KingBlackCow <sgs1159@naver.com>
2022-03-21 23:46:10 +0900

    first-init

---
+++ string.rb
@@ -0,0 +1,15 @@
+first_name = "JS"
+last_name = "LEE"
+puts "My first name is #{first_name} and my last name is #{last_name}"
+puts 'My first name is #{first_name} and my last name is #{last_name}'
+full_name = "#{first_name} #{last_name}"
+puts full_name
+puts full_name.length
+puts full_name.reverse
+puts full_name.capitalize
+puts full_name.empty?
+puts full_name.nil?
+puts nil.nil?
+
+sentence = "Welcome to the jungle"
+puts sentence.sub("the jungle", "utopia")
