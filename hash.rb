b75b8aa4c9ade6ce50287fc5d5706c9eb1f89759
KingBlackCow <sgs1159@naver.com>
2022-04-07 18:00:36 +0900

    feat: hash 추가

---
+++ hash.rb
@@ -0,0 +1,15 @@
+my_details = {'name' => 'mashrur', 'favcolor' => 'red'}
+my_details["favcolor"]
+
+myhash = {a: 1, b: 2, c: 3, d: 4}
+myhash[:c]
+myhash[:d] = 7
+myhash[:name] = "Mashrur"
+myhash.delete(:d)
+
+myhash.keys
+myhash.values
+
+myhash.each { |somekey, somevalue| puts somevalue }
+myhash.each { |somekey, somevalue| puts "The key is #{somekey} and the value is #{somevalue}" }
+myhash.select { |k, v| v.odd? }
